7c66a74649c2d7d334ca6f99c9902ef9e5462316
kala <15959373098@163.com>
2020-10-16 22:16:33 +0800

    2020年10月16日更新

---
+++ src/com/exercise/exercise22.java
@@ -0,0 +1,14 @@
+package com.exercise;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class exercise22 {
+    public List<String> generateParenthesis(int n) {
+        List<String> list = new ArrayList<>();
+
+        valid
+
+        return list;
+    }
+}
