03330c46491206f528161f69273afd295f32c002
kala <15959373098@163.com>
2020-10-24 17:57:06 +0800

    2020年10月24日更新

--- src/com/exercise/exercise28.java
+++ src/com/exercise/exercise28.java
@@ -1,13 +1,15 @@
 package com.exercise;
 
 public class exercise28 {
-    public int strStr(String haystack, String needle) {
-        int n = haystack.length(), L = needle.length();
-        if (L==0) return 0;
+    public static int strStr(String haystack, String needle) {
+        int L = needle.length(), n = haystack.length();
+        if (L == 0) {
+            return 0;
+        }
         int pn = 0;
         while (pn < n - L + 1) {
             while (pn < n - L + 1 && haystack.charAt(pn) != needle.charAt(0)) pn++;
-            int pL = 0, currLen = 0;
+            int currLen = 0, pL = 0;
             while (pL < L && pn < n && haystack.charAt(pn) == needle.charAt(pL)) {
                 pL++;
                 pn++;
---
+++ src/com/exercise/exercise32.java
@@ -0,0 +1,31 @@
+package com.exercise;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class exercise32 {
+    public static int longestValidParentheses(String s) {
+        int ans = 0;
+        Deque<Integer> stack = new LinkedList<>();
+        stack.push(-1);
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.isEmpty()) {
+                    stack.push(i);
+                } else {
+                    ans = Math.max(ans, i - stack.peek());
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(longestValidParentheses("(()"));
+    }
+}
