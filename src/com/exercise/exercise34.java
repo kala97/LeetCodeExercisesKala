b50f2c6d78d8e0aa12cfa17dd339dc0f1bd35334
Kala <15959373098@163.com>
2020-10-29 17:52:40 +0800

    2020年10月29日更新

---
+++ src/com/exercise/exercise34.java
@@ -0,0 +1,13 @@
+package com.exercise;
+
+public class exercise34 {
+    public int[] searchRange(int[] nums, int target) {
+        int[] index = {-1,-1};
+        int n = nums.length;
+        int l = 0,r = n-1;
+        while (r<=l){
+
+        }
+        return index;
+    }
+}
