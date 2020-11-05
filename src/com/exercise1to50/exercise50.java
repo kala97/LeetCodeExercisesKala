73970339d34774962cb397227416270a44006dd5
kala <15959373098@163.com>
2020-11-05 23:35:22 +0800

    2020年11月5日更新

--- src/com/exercise1to50/exercise50.java
+++ src/com/exercise1to50/exercise50.java
@@ -1,5 +1,17 @@
 package com.exercise1to50;
 
 public class exercise50 {
+    public double quickMul(double x, long N) {
+        if (N == 0) {
+            return 1.0;
+        }
+        double y = quickMul(x, N / 2);
+        return N % 2 == 0 ? y * y : y * y * x;
+    }
 
+    public double myPow(double x, int n) {
+        long N = n;
+        return N >= 0 ? quickMul(x, N) : 1.0 / quickMul(x, -N);
+
+    }
 }
---
+++ src/com/exercise51to100/exercise58.java
@@ -0,0 +1,17 @@
+package com.exercise51to100;
+
+public class exercise58 {
+    public static int lengthOfLastWord(String s) {
+        int n = s.length(), length = 0;
+        int end = n - 1, start = 0;
+        while (end >= 0 && s.charAt(end) == ' ') end--;
+        if (end < 0) return 0;
+        start = end;
+        while (start >= 0 && s.charAt(start) != ' ') start--;
+        return end-start;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLastWord(new String("Hello World")));
+    }
+}
