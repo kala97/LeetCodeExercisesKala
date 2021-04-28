df1074d97b897376a38dd76c88212cba14ea3d7b
卡拉 <cky713237>
2021-04-28 10:36:07 +0800

    update

--- src/com/kala/datastructure/search/BinarySearchTree.java
+++ src/com/kala/datastructure/search/BinarySearchTree.java
@@ -1,5 +1,6 @@
 package com.kala.datastructure.search;
 
+import com.kala.common.TreeNode;
 /**
  * 二叉搜索树的各种方法
  */
@@ -8,7 +9,7 @@ public class BinarySearchTree {
     /**
      * 搜索方法
      */
-    public boolean searchBST(){
+    public boolean searchBST(TreeNode){
         return false;
     }
     /**
